0cf02833bae3e2ee529b53ca1fb6ddec14350d71
Sasiwimon <112236007+ganksomzbf@users.noreply.github.com>
2022-10-08 11:35:10 +0700

    Add files via upload

---
+++ src/pages/Home.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <Navbar />
+        <section class="home">
+            <div class="image">
+                <img src="images/profile..png" alt="">
+            </div>
+            <div class="content">
+                <h3>hi, i am {{name}}</h3>
+                <span>{{motto}}</span>
+                <p>{{content}}</p>
+                <router-link to="/about" class="btn"> about me <i class="fas fa-user"></i> </router-link>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue';
+export default {
+    components: { Navbar },
+    data() {
+        return {
+            name: 'Sasiwimon Masing',
+            motto: 'Belive the Impossible is Possible.',
+            content: 'More 5 Years Of Experienced Developer With Front-End And Back-End .NET Programming In The Information Technology And Service Industry. Skilled In HTML, CSS, Javascript, Angular, React, MVC, C# And ORACLE, SQL Database Tools. A Strong Engineering Professional With A Bachelor Of Engineering (BE) Focused On Computer Engineering From Bangkok University.',
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/Projects.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <Navbar />
+        <section class="projects">
+            <h1 class="heading"> <span>my</span> projects </h1>
+            <h3>{{project_name}}</h3>
+            <p>{{project_detail}}</p>
+            <div class="box-container">
+                <iframe width="720" height="405" src="https://www.youtube.com/embed/603Hc7ycIS0"
+                    title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
+            gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue';
+export default {
+    components: { Navbar },
+    data() {
+        return {
+            project_name: 'LAZ1',
+            project_detail: '5 ตัวท็อปจากรายการเฟ้นหาไอดอลชื่อดัง LAZ iCON สู่การเดบิวต์เป็นศิลปินบอยกรุ๊ปน้องใหม่มาแรง LAZ1 พร้อมมาเขย่าวงการ T-Pop ด้วยซิงเกิลแรก Taste Me ที่ส่งมาให้แฟนคลับได้ลิ้มลองความรักในแบบฉบับของพวกเขา',
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
